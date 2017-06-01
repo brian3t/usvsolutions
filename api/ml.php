29bd2976c1d5788f3d59b1cd53beb0ec4d40bb79
brian3t <ngxtri@gmail.com>
2017-05-31 20:55:31 -0700

    USV

--- api/ml.php
+++ api/ml.php
@@ -5,9 +5,13 @@ header('Content-Type: application/json');
 
 
 extract($_POST);
-if (!isset($from) || !isset($message) || !isset($key) || ($key !== 'f>6Ea@/N7e')) {
-    echo json_encode(['status' => 'failed', 'error' => 'Missing from or message or wrong key']);
+if(!isset($from) || !isset($message) || !isset($key) || ($key!=='f>6Ea@/N7e'))
+{
+    echo json_encode(['status' => 'failed','error' => 'Missing from or message or wrong key']);
     exit();
 }
 
-mail('ngxtri@gmail.com', "Mail from api " . $_SERVER['REMOTE_ADDR'] . " " . $_SERVER['HTTP_HOST'], $message);
+if(mail('ngxtri@gmail.com',"Mail from api " . $_SERVER['REMOTE_ADDR'] . " " . $_SERVER['HTTP_HOST'],$message))
+{
+    echo json_encode(['status' => 'ok']);
+}
